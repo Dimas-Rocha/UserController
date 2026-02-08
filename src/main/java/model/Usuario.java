da33ea5411a955fae23099599fc284304c0a38ca
Dimas Rocha <dimas.dasp@gmail.com>
2026-02-07 21:49:18 -0300

    model package add

---
+++ src/main/java/model/Usuario.java
@@ -0,0 +1,30 @@
+package model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "usuarios")
+@Data
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String nome;
+
+    private String senha;
+
+    @Column(name = "data_criacao")
+    private LocalDateTime dataCriacao = LocalDateTime.now();
+
+    @Column(name = "data_atualizacao")
+    private LocalDateTime dataAtualizacao = LocalDateTime.now();
+}
